63fcc0475be3347652ce529a06ca3de5c0d07a1d
Jack Laird <jack.laird0@gmail.com>
2018-10-09 22:29:23 -0600

    Add active class name to navlinks and jobn title

--- src/Containers/NavBar/NavBar.js
+++ src/Containers/NavBar/NavBar.js
@@ -4,38 +4,39 @@ import './NavBar.css';
 
 class NavBar extends Component {
   constructor() {
-    super()
+    super();
   }
 
   render () {
     return (
       <div className="nav-bar">
         <div className= "profile-name-info">
-          <p className="name">Jack Laird</p>
+          <p className="name">JACK LAIRD</p>
+          <p className="title">software developer</p>
         </div>
         <div className="nav-link-container">
-          <NavLink to='/'> 
-            <div className="nav-bar-link-container link-home">
+          <NavLink exact to='/' className="nav-bar-link-container link-home" activeClassName="link-home-active"> 
+            <div>
               HOME 
             </div>
           </NavLink>
-          <NavLink to='/portfolio'> 
-            <div className="nav-bar-link-container link-portfolio">
-            PORTFOLIO 
+          <NavLink to='/portfolio' className="nav-bar-link-container link-portfolio" activeClassName="link-portfolio-active"> 
+            <div>
+              PORTFOLIO 
             </div>
           </NavLink>
-          <NavLink to='/about'> 
-            <div className="nav-bar-link-container link-about">
+          <NavLink to='/about' className="nav-bar-link-container link-about" activeClassName="link-about-active"> 
+            <div>
             ABOUT 
             </div>
           </NavLink>
-          <NavLink to='/resume'> 
-            <div className="nav-bar-link-container link-resume">
+          <NavLink to='/resume' className="nav-bar-link-container link-resume" activeClassName="link-resume-active"> 
+            <div>
             RESUME 
             </div>
           </NavLink>
-          <NavLink to='/contact'> 
-            <div className="nav-bar-link-container link-contact">
+          <NavLink to='/contact' className="nav-bar-link-container link-contact" activeClassName="link-contact-active"> 
+            <div>
             CONTACT 
             </div>
           </NavLink>
