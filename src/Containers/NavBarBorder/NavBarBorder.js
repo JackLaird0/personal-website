b74a0b545beb58662784ea93a50c6fd498b31554
Jack Laird <jack.laird0@gmail.com>
2018-10-09 20:59:34 -0600

    Add border to bottom of navbar

---
+++ src/Containers/NavBarBorder/NavBarBorder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './NavBarBorder.css';
+
+class NavBarBorder extends Component {
+  constructor() {
+    super()
+  }
+
+  render () {
+    return (
+      <div className="nav-border">
+      </div>
+    )
+  }
+}
+
+export default NavBarBorder;
