c175b18f511295f2f5f0961cc5d971352ae999d7
Jack Laird <jack.laird0@gmail.com>
2018-10-11 16:55:26 -0600

    Start with jsx for contact info

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Contact.css';
 
 class Contact extends Component {
   constructor() {
@@ -7,8 +8,17 @@ class Contact extends Component {
 
   render() {
     return (
-      <div>
-        
+      <div className="contact-page">
+        <h1 className="contact-greeting">
+          reach out
+        </h1>
+        <div className="contact-card">
+          <p className="contact-name">
+            Jack Laird
+          </p>
+          <p>Email: Jack.laird0@gmail.com</p>
+          <p>Phone: 719-290-7764</p>
+        </div>
       </div>
     )
   }
